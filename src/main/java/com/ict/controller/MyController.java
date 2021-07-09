22be3ee779c87e03cb669a016c597d88845a15e1
choi970702 <choi970702@gmail.com>
2021-07-09 17:44:26 +0900

    1. 21/07/09

---
+++ src/main/java/com/ict/controller/MyController.java
@@ -0,0 +1,54 @@
+package com.ict.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ict.service.CalcService;
+
+@Controller
+public class MyController 
+{
+	// CalcService calc = new CalcService();
+	// root-context.xml에서 객체 생성.
+	@Autowired
+	private CalcService calc;
+	
+	public void setCalc(CalcService calc) 
+	{
+		this.calc = calc;
+	}
+	@RequestMapping("add.do")
+	public ModelAndView addCommand()
+	{
+		ModelAndView mv = new ModelAndView("result");
+		int result = calc.plus();
+		mv.addObject("result", result);
+		return mv;
+	}
+	@RequestMapping("sub.do")
+	public ModelAndView subCommand()
+	{
+		ModelAndView mv = new ModelAndView("result");
+		int result = calc.minus();
+		mv.addObject("result", result);
+		return mv;
+	}
+	@RequestMapping("mul.do")
+	public ModelAndView mulCommand()
+	{
+		ModelAndView mv = new ModelAndView("result");
+		int result = calc.multi();
+		mv.addObject("result", result);
+		return mv;
+	}
+	@RequestMapping("div.do")
+	public ModelAndView divCommand()
+	{
+		ModelAndView mv = new ModelAndView("result");
+		int result = calc.divide();
+		mv.addObject("result", result);
+		return mv;
+	}
+}
